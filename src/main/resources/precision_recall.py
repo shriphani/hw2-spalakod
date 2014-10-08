7f561cba21a93a6fc22f7bdc5796a4d5d844acc5
shriphani <spalakod@cs.cmu.edu>
2014-10-07 20:09:39 -0400

    added eval script

---
+++ src/main/resources/precision_recall.py
@@ -0,0 +1,40 @@
+'''
+Measure P and R given ground truth and other data
+'''
+
+import argparse
+
+def compare(truth_file, test_file):
+	with open(truth_file, 'r') as truth_file_handle, open(test_file, 'r') as test_file_handle:
+		truth_lines = set(truth_file_handle.readlines())
+		test_lines = set(test_file_handle.readlines())
+
+		truths_retrieved = set.intersection(truth_lines, test_lines)
+
+		precision = len(truths_retrieved) / float(len(test_lines))
+		recall = len(truths_retrieved) / float(len(truth_lines))
+		f1_score = 2.0 * (precision * recall) / (precision + recall)
+
+
+		return precision, recall, f1_score
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument(
+		'--truth',
+		dest = 'truth', 
+	)
+	parser.add_argument(
+		'--test',
+		dest = 'test', 
+	)
+	
+	args = parser.parse_args()
+
+	precision, recall, f1_score = compare(args.truth, args.test)
+
+	print 'Precision:', precision
+	print 'Recall:', recall
+	print 'F1 Score:', f1_score
